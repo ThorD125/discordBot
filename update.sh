94dcd186932cd32b928c7e467ccc2727ab13b935
thorpizero <pizero@thor-demeestere.be>
2023-07-31 22:57:35 +0200

    update

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@
 git pull
 
 # Step 2: Terminate the running script
-# Change "index.py" to the actual name of your Python script if different
-running_script="runningscript.py"
+# Change "./index.py" to the actual name of your Python script if different
+running_script="./index.py"
 pids=$(ps aux | grep "$running_script" | grep -v grep | awk '{print $2}')
 if [ -n "$pids" ]; then
   echo "Terminating running instances of $running_script..."
@@ -14,4 +14,4 @@ if [ -n "$pids" ]; then
 fi
 
 # Step 3: Execute the Python3 script
-python3 "$running_script"
+python3 "$running_script" &
