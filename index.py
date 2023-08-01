6931dbbbe12640fe4a3e601f409236755233bd57
ther <thor.demeestere@telenet.be>
2023-08-01 17:25:14 +0200

    update

--- index.py
+++ index.py
@@ -32,8 +32,6 @@ async def on_message(message):
     if message.content.startswith('pong'):
         await message.channel.send('ping !')
 
-    print(get_list_from_mariadb())
-
     if message.content.startswith('dbtest'):
         await message.channel.send(get_list_from_mariadb())
 
