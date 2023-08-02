9fcd3c0790c867f7622e7f2ab71753b1ebd918d4
ther <thor.demeestere@telenet.be>
2023-08-02 21:13:43 +0200

    update

--- index.py
+++ index.py
@@ -61,7 +61,14 @@ async def dnslookup(ctx, url=None):
 
 @client.command()
 async def generatepassword(ctx, amount=20):
-    await ctx.send(os.popen(f"cat /dev/urandom | tr -dc 'A-Za-z0-9!?><,./\-_=+~:;*&^%$#@' | head -c {amount})").read())
+    await ctx.send(os.popen(f"cat /dev/urandom | tr -dc 'A-Za-z0-9!?><,./\-_=+~:;*&^%$#@()[]' | head -c {amount}").read())
+
+
+@client.command()
+async def test(ctx):
+    username = ctx.author.name
+
+    await ctx.send("hi" + username)
 
 
 client.run(TOKEN)
