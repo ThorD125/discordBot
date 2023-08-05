33bf223945136dfbd27397d4682f7204ad7e2a6f
ther <thor.demeestere@telenet.be>
2023-08-05 23:18:46 +0200

    update

--- util/py/helper.py
+++ util/py/helper.py
@@ -5,7 +5,7 @@ def log(message):
     os.popen(f"wall {message}")
 
 def bashCommand(command):
+    print(f"1Command: {command}")
     output = subprocess.check_output(command, shell=True, text=True)
-    print(f"3Command: {output}")
-    # log(f"Command: {command}\nOutput: {output}")
+    print(f"2Output: {output}")
     return output
