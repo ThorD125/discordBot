2e612dd7a72860c0afc379c5d4ddbf7bfd625da2
ther <thor.demeestere@telenet.be>
2023-08-05 23:26:05 +0200

    update

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import subprocess
 
 from util.py.helper import log, bashCommand
 from util.py.env import loadEnv
@@ -9,6 +10,9 @@ env = loadEnv()
 
 bot = discord.Bot()
 
+def bashCommand(command):
+    return subprocess.check_output(command, shell=True, text=True)
+
 
 @bot.event
 async def on_ready():
@@ -17,15 +21,13 @@ async def on_ready():
 
 @bot.slash_command(description="Pong a URL")
 async def ping(ctx, url=None):
-    log(f"pinging {url}")
     if url is None:
         await ctx.respond("pong!")
     else:
-        log(f"thurl: {url}")
-        await ctx.respond(await bashCommand(f"ping -c 4 {url} "))
+        await ctx.respond(bashCommand(f"ping -c 4 {url} "))
 
 
-@bot.slash_command( description="Trace a URL")
+@bot.slash_command(description="Trace a URL")
 async def traceroute(ctx, url=None):
     if url is None:
         await ctx.respond("Enter a URL to trace!")
@@ -46,13 +48,13 @@ async def generatepassword(ctx, amount=20):
     await ctx.respond(bashCommand(f"cat /dev/urandom | tr -dc 'A-Za-z0-9!?><,./\-_=+~:;*&^%$#@()[]' | head -c {amount}"))
 
 
-@bot.slash_command(description="Updating the bot", guild_ids=[env["SERVERID"]])
+@bot.slash_command(description="Updating the bot")
 async def update(ctx):
     await ctx.respond("Updating!")
     log("Updating!")
     bashCommand(f"./update.sh")
 
-@bot.slash_command(description="Restart the bot", guild_ids=[env["SERVERID"]])
+@bot.slash_command(description="Restart the bot")
 async def restart(ctx):
     await ctx.respond("Restarting!")
     log("Restarting!")
