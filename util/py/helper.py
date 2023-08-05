8481d792bbf5f389cb45ffd36d5b615607aeecea
ther <thor.demeestere@telenet.be>
2023-08-05 23:45:42 +0200

    update

--- util/py/helper.py
+++ util/py/helper.py
@@ -4,7 +4,7 @@ import subprocess
 def log(message):
     os.popen(f"wall {message}")
 
-async def bashCommand(command):
+def bashCommand(command):
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     process.wait()
     return process.stdout.read().decode("utf-8")
