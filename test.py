4f7470f0a6279a5612b708d96f18c5472626db77
ther <thor.demeestere@telenet.be>
2023-08-05 23:16:35 +0200

    update

---
+++ test.py
@@ -0,0 +1,3 @@
+from util.py.helper import log, bashCommand
+
+print(bashCommand("ping -c 4 google.com"))
--- util/py/helper.py
+++ util/py/helper.py
@@ -5,7 +5,7 @@ def log(message):
     os.popen(f"wall {message}")
 
 def bashCommand(command):
-    output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    output.wait()
-    log(f"Command: {command}\nOutput: {output}")
-    return output.stdout.read().decode("utf-8")
+    output = subprocess.check_output(command, shell=True, text=True)
+    print(f"3Command: {output}")
+    # log(f"Command: {command}\nOutput: {output}")
+    return output
