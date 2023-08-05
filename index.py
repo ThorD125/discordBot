ac97283473621fe272d933dbebf12789547bba38
ther <thor.demeestere@telenet.be>
2023-08-05 23:47:29 +0200

    update

--- index.py
+++ index.py
@@ -25,38 +25,41 @@ async def ping(ctx, url=None):
         await ctx.respond(bashCommand(f"ping -c 4 {url}"))
 
 
-# @bot.slash_command(description="Trace a URL")
-# async def traceroute(ctx, url=None):
-#     if url is None:
-#         await ctx.respond("Enter a URL to trace!")
-#     else:
-#         await ctx.respond(bashCommand(f"traceroute {url} "))
+@bot.slash_command(description="Trace a URL")
+async def traceroute(ctx, url=None):
+    if url is None:
+        await ctx.respond("Enter a URL to trace!")
+    else:
+        await ctx.defer()
+        await ctx.respond(bashCommand(f"traceroute {url} "))
 
 
-# @bot.slash_command(description="DNS lookup")
-# async def dnslookup(ctx, url=None):
-#     if url is None:
-#         await ctx.respond("Enter a URL to trace!")
-#     else:
-#         await ctx.respond(bashCommand(f"dig {url}"))
+@bot.slash_command(description="DNS lookup")
+async def dnslookup(ctx, url=None):
+    if url is None:
+        await ctx.respond("Enter a URL to trace!")
+    else:
+        await ctx.defer()
+        await ctx.respond(bashCommand(f"dig {url}"))
 
 
-# @bot.slash_command(description="Generate a random password")
-# async def generatepassword(ctx, amount=20):
-#     await ctx.respond(bashCommand(f"cat /dev/urandom | tr -dc 'A-Za-z0-9!?><,./\-_=+~:;*&^%$#@()[]' | head -c {amount}"))
+@bot.slash_command(description="Generate a random password")
+async def generatepassword(ctx, amount=20):
+    await ctx.defer()
+    await ctx.respond(bashCommand(f"cat /dev/urandom | tr -dc 'A-Za-z0-9!?><,./\-_=+~:;*&^%$#@()[]' | head -c {amount}"))
 
 
-# @bot.slash_command(description="Updating the bot")
-# async def update(ctx):
-#     await ctx.respond("Updating!")
-#     log("Updating!")
-#     bashCommand(f"./update.sh")
+@bot.slash_command(description="Updating the bot")
+async def update(ctx):
+    await ctx.respond("Updating!")
+    log("Updating!")
+    bashCommand(f"./update.sh")
 
-# @bot.slash_command(description="Restart the bot")
-# async def restart(ctx):
-#     await ctx.respond("Restarting!")
-#     log("Restarting!")
-#     bashCommand(f"./util/sh/restart.sh")
+@bot.slash_command(description="Restart the bot")
+async def restart(ctx):
+    await ctx.respond("Restarting!")
+    log("Restarting!")
+    bashCommand(f"./util/sh/restart.sh")
 
 
 
