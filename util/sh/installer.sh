301b8850df6c32c49ef7a2a442a09a1367a18c9d
ther <thor.demeestere@telenet.be>
2023-12-14 16:33:45 +0100

    update

--- util/sh/installer.sh
+++ util/sh/installer.sh
@@ -2,3 +2,6 @@
 
 apt install traceroute dnsutils -y
 pip install -r requirements.txt
+
+sudo cp /home/ther/discordBot/discord-bot.service /etc/systemd/system/
+sudo systemctl daemon-reload
--- util/sh/restart.sh
+++ util/sh/restart.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-running_script="./index.py"
-
-pids=$(ps aux | grep "$running_script" | grep -v grep | awk '{print $2}')
-if [ -n "$pids" ]; then
-  echo "Terminating running instances of $running_script..."
-  kill -9 $pids
-  sleep 1
-fi
-
-python3 "$running_script" &
+sudo systemctl restart discord-bot
